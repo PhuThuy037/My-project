90d3f789e9f97dd3618d870e8c104c950a10e79c
Thuy Nguyen <phuthuynguyenn@gmail.com>
2024-09-30 15:01:49 +0700

    initial project

---
+++ src/test/java/com/user_mangement/User_Mangement/UserMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_mangement.User_Mangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
